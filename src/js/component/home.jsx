b739a1e68fa0f779988814a8fc4b26107dfa273e
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2022-12-09 18:04:19 +0000

    ejercicio planteado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,45 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
+
+  
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	
+		const [seconds, setSeconds] = useState(0);
+		const [secondSeconds, setSecondSeconds] = useState(0);
+		const [thirdSeconds, setThirdSeconds] = useState(0);
+		const [fourthSeconds, setFourthSeconds] = useState(0)
+	  
+		useEffect(() => {
+		  const interval = setInterval(() => {
+			setSeconds(seconds => seconds + 1);
+		  }, 1000);
+		  return () => clearInterval(interval);
+		}, []);
+	  
+		useEffect(() => {
+			if (seconds > 9){
+				setSecondSeconds(secondSeconds => secondSeconds + 1)
+				setSeconds(seconds => seconds = 0)
+				if (secondSeconds > 8 ){
+					setThirdSeconds(thirdSeconds => thirdSeconds + 1)
+					setSecondSeconds(secondSeconds => secondSeconds = 0)
+					if (thirdSeconds > 8){
+						setFourthSeconds(fourthSeconds => fourthSeconds + 1)
+						setThirdSeconds(thirdSeconds => thirdSeconds = 0)
+						
+					}
+				}
+			}
+		})
+
+		return (
+		  <div className="text-center fs-2 ">
+					<i className="fa-solid fa-clock"></i> {fourthSeconds}{thirdSeconds}{secondSeconds}{seconds} 			
+		  </div>
+		);
+	  };
 
 export default Home;
