4a578e96cc8b983dbeae94d94308992511a2ef26
MATIAS EXEQUIEL CECCI <matias_cecci@hotmail.com>
2023-01-16 11:20:53 +0000

    agregado de boton y estilo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,6 @@
 import React, {useState, useEffect} from "react";
 
 
-
-
-  
-
 const Home = () => {
 	
 		const [seconds, setSeconds] = useState(0);
@@ -35,10 +31,36 @@ const Home = () => {
 			}
 		})
 
+	
+
 		return (
-		  <div className="text-center fs-2 ">
-					<i className="fa-solid fa-clock"></i> {fourthSeconds}{thirdSeconds}{secondSeconds}{seconds} 			
-		  </div>
+
+		  	<div className="container bg-light mx-auto w-50 mt-5 border border-dark rounded p-5" >
+				<div className="container w-50 text-center">
+					<h2 className="text center mb-5 fs-1">Simple Counter - React Hook</h2>
+				</div>
+				<div className="container w-50 fs-2 border border-dark rounded text-center ">
+					<div>
+						<i className="fa-regular fa-clock" >  </i> 
+					</div>
+					<div>
+						{fourthSeconds}
+						{thirdSeconds}
+						{secondSeconds}
+						{seconds}
+					</div>
+				</div>	
+				<div className="d-flex justify-content-center mt-3">
+					<button type="button" onClick={() => {
+						setFourthSeconds(fourthSeconds => fourthSeconds = 0)
+						setThirdSeconds(thirdSeconds => thirdSeconds = 0)
+						setSecondSeconds(secondSeconds => secondSeconds = 0)
+						setSeconds(seconds => seconds = 0)}} 
+						className="btn btn-warning">
+							Reset Clock
+					</button>
+				</div>
+				</div>
 		);
 	  };
 
